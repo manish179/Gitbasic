0cbd9158171f5785e14a95f4f1f7a3d0a720d251
manish179 <manishshrestha179@gmail.com>
2018-05-27 14:42:18 +0545

    master

--- app/src/main/java/com/example/manish/gitbasic/MainActivity.java
+++ app/src/main/java/com/example/manish/gitbasic/MainActivity.java
@@ -15,7 +15,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(MainActivity.this,"user created",Toast.LENGTH_LONG).show();
-        Toast.makeText(MainActivity.this,"sigin gara",Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this,"master ko toast",Toast.LENGTH_LONG).show();
+
 
 
 
