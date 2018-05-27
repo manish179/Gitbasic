9af44410de0a8281d45cc9999d73a13c29139fd9
manish179 <manishshrestha179@gmail.com>
2018-05-27 14:36:00 +0545

    added toast

--- app/src/main/java/com/example/manish/gitbasic/MainActivity.java
+++ app/src/main/java/com/example/manish/gitbasic/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.manish.gitbasic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this,"user created",Toast.LENGTH_LONG).show();
+
+
 
     }
 }
