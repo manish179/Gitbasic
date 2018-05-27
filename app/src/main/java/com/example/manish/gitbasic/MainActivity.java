ed50c13d96a19edcef80a51faf7e33c13127b7b8
liamoar <avishr44@gmail.com>
2018-05-27 14:43:27 +0545

    signup ma username rakhe

--- app/src/main/java/com/example/manish/gitbasic/MainActivity.java
+++ app/src/main/java/com/example/manish/gitbasic/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(MainActivity.this,"user created",Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this,"avi toast",Toast.LENGTH_LONG).show();
+
 
 
 
